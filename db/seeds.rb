4c5ddab10519c6c6b7c1f273a8ec8125005d50fd
Jordan <jcharrison12@gmail.com>
2021-06-16 12:59:36 -0500

    seeds db with Products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "canvases", price: 20, image_url: "https://images.ctfassets.net/f1fikihmjtrp/3CDdug9IYkzm0AMUACUmAW/e8ddf90393326c1b00602b611953865e/07008-group1-4ww.jpg?q=80&w=250&fm=webp", description: "A multi-pack of decent canvas panels")
+Product.create(name: "paints", price: 10, image_url: "fakeurl.jpg", description: "Oil paints in all colors of the rainbow")
